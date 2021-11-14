e724047d26d8f56c53dae769366642222d5b15d4
mikeymusch <83678747+mikeymusch@users.noreply.github.com>
2021-11-14 21:11:23 +1100

    removed createAssetMenu attribute

--- Runtime/Condition.cs
+++ Runtime/Condition.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "FSM/Condition")]
 public abstract class Condition : ScriptableObject
 {
     public abstract bool Check(StateMachine stateMachine);
